b6ee0d65afb12b09665364b2306e6612c4974dc5
gkcsd <kaleg2025@gmail.com>
2021-04-19 23:50:13 +0530

    primitive sec example added

---
+++ src/basicjavaprograms/PrimitiveTypeTwo.java
@@ -0,0 +1,17 @@
+package basicjavaprograms;
+
+public class PrimitiveTypeTwo {
+    public static void main(String[] args) {
+        boolean isValid = true;
+
+        byte marks = 90;
+
+        float pi = 3.14f;
+
+        float div = 15.0f/ 4.0f;
+
+        long views = 10000000000L;
+
+        char gender = 'M';
+    }
+}
