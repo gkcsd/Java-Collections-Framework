30e58ad7e0015dedfb6553755c598ceadfd5e414
gkcsd <kaleg2025@gmail.com>
2021-04-20 22:37:03 +0530

    PrimitiveTypeThree example added

---
+++ src/basicjavaprograms/PrimitiveTypeThree.java
@@ -0,0 +1,12 @@
+package basicjavaprograms;
+
+public class PrimitiveTypeThree {
+    public static void main(String[] args) {
+        int x1 = 10;
+        int x2 = x1;
+
+        x2 = 30;
+        System.out.println(x1);
+        System.out.println(x2);
+    }
+}
