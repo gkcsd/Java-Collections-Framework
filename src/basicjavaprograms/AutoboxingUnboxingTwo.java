85a868515f5625805c2aff645ed46c0f94efae5b
gkcsd <kaleg2025@gmail.com>
2021-04-21 22:11:28 +0530

    AutoboxingUnboxing second example added

---
+++ src/basicjavaprograms/AutoboxingUnboxingTwo.java
@@ -0,0 +1,13 @@
+package basicjavaprograms;
+
+public class AutoboxingUnboxingTwo {
+    public static void main(String[] args) {
+        Integer x = 400, x2 = 400;
+
+        if (x == x2) {
+            System.out.println("Same");
+        } else {
+            System.out.println("Not Same");
+        }
+    }
+}
