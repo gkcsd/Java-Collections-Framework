a461cacebbbd01ad00d3e6ee5267e8720b4936cf
gkcsd <kaleg2025@gmail.com>
2021-04-21 22:10:21 +0530

    AutoboxingUnboxing first example added

---
+++ src/basicjavaprograms/AutoboxingUnboxing.java
@@ -0,0 +1,13 @@
+package basicjavaprograms;
+
+public class AutoboxingUnboxing {
+    public static void main(String[] args) {
+        int x1 = 10;
+        Integer x2 = x1;
+        int x3 = x2;
+
+        System.out.println(x1);
+        System.out.println(x2);
+        System.out.println(x3);
+    }
+}
