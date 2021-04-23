960ad78ec2f150be41e217048e44fde24cfc4efa
gkcsd <kaleg2025@gmail.com>
2021-04-23 23:20:56 +0530

    break example added

---
+++ src/BreakKeyword.java
@@ -0,0 +1,11 @@
+public class BreakKeyword {
+    public static void main(String[] gkcsd) {
+        int i;
+        for (i = 0; i < 5; i++) {
+            if(i == 2) break;
+
+            System.out.println(i);
+        }
+        System.out.println(i);
+    }
+}
