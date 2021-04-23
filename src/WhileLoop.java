ebe673c1e038b30590edb132e1492ce16b6ae3d5
gkcsd <kaleg2025@gmail.com>
2021-04-23 21:54:51 +0530

    while loop added

---
+++ src/WhileLoop.java
@@ -0,0 +1,9 @@
+public class WhileLoop {
+    public static void main(String[] gkcsd) {
+        int i = 0;
+        while(i < 10) {
+            System.out.println("I am best programmer in the world!");
+            i++;
+        }
+    }
+}
