fbc8ef2ae04511b42f8326fb366ba1417c55950a
gkcsd <kaleg2025@gmail.com>
2021-04-23 23:21:30 +0530

    ContinueKeyword example added

---
+++ src/ContinueKeyword.java
@@ -0,0 +1,12 @@
+public class ContinueKeyword {
+    public static void main(String[] gkcsd) {
+
+        int i;
+        for (i = 0; i < 5; i++) {
+            if (i == 3) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
