4e74768feb7a17e8bf7499159424181ece32f5a7
gkcsd <kaleg2025@gmail.com>
2021-04-23 21:48:06 +0530

    for loop added

---
+++ src/ForLoop.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ForLoop {
+    public static void main(String[] gkcsd) {
+        Scanner obj = new Scanner(System.in);
+         int n = obj.nextInt();
+        for (int i = 0; i < n; i ++) {
+            System.out.println(i);
+        }
+    }
+}
