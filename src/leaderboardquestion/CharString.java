2f35be448606970a6cba7572734d1049826b0a0d
gkcsd <kaleg2025@gmail.com>
2021-04-27 15:29:10 +0530

    Char string example added

---
+++ src/leaderboardquestion/CharString.java
@@ -0,0 +1,9 @@
+package leaderboardquestion;
+
+public class CharString {
+    public static void main(String args[]) {
+        char chars[] = {'a', 'b', 'c'};
+        String s = new String(chars);
+        System.out.println(s);
+    }
+}
