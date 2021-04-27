61a86b5889d9818149f6ca4433d790984fe7b033
gkcsd <kaleg2025@gmail.com>
2021-04-27 15:30:14 +0530

    SolThree added

---
+++ src/leaderboardquestion/SolThree.java
@@ -0,0 +1,18 @@
+package leaderboardquestion;
+
+class GFG {
+        int x;
+        String str;
+        char ch ;
+}
+
+class SolThree {
+    public static void main (String[] args) {
+            GFG obj = new GFG();
+            obj.x = 20;
+            obj.str = "Java";
+            obj.ch = 'G';
+
+            System.out.print(obj.x + " " + obj.str + " " + obj.ch);
+    }
+}
