1bee832be563ca23cce603fa132940483d4cdce3
gkcsd <kaleg2025@gmail.com>
2021-04-27 15:30:25 +0530

    SolTwo added

---
+++ src/leaderboardquestion/SolTwo.java
@@ -0,0 +1,9 @@
+package leaderboardquestion;
+
+public class SolTwo {
+        public static void main(String args[]) {
+            int arr[] = { 65, 66, 67, 68};
+            String s = new String(arr, 1, 3);
+            System.out.println(s);
+        }
+}
