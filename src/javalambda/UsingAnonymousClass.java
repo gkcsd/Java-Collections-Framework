273e726d60f3822cbf447973826bb7a0284740af
gkcsd <kaleg2025@gmail.com>
2021-05-11 23:56:31 +0530

    UsingAnonymousClass example added

---
+++ src/javalambda/UsingAnonymousClass.java
@@ -0,0 +1,30 @@
+package javalambda;
+
+import java.io.*;
+import java.util.*;
+import java.util.function.*;
+
+public class UsingAnonymousClass {
+    static void printCond(Collection<Integer> C, Predicate<Integer> p)
+    {
+        for(Integer x: C)
+        {
+            if(p.test(x))
+                System.out.print(x+" ");
+        }
+    }
+    public static void main (String[] args) {
+
+        Predicate<Integer> myPredEven= new Predicate<Integer>(){
+
+            public boolean test(Integer x)
+            {
+                return x%2==0;
+            }
+        };
+
+        List<Integer> al= new ArrayList<>(Arrays.asList(10,5,20,7,30));
+
+        printCond(al,myPredEven);
+    }
+}
