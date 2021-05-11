32ee1496d685d5e53cd1131f9b7f622e68e306b2
gkcsd <kaleg2025@gmail.com>
2021-05-11 23:53:48 +0530

    ComparatorMethodReference example added

---
+++ src/javalambda/ComparatorMethodReference.java
@@ -0,0 +1,21 @@
+package javalambda;
+
+import java.io.*;
+import java.util.*;
+import java.util.function.*;
+
+public class ComparatorMethodReference {
+
+    public static void main (String[] args) {
+
+        String a[] = {"GFG","IDE","Courses"};
+        String b[] = {"gfg","ide","courses"};
+
+//        if(Arrays.equals(a,b,(x1,x2)->String.compareToIgnoreCase(x1,x2)))
+//            System.out.println("Yes");
+//        else
+//            System.out.println("No");
+
+    }
+
+}
