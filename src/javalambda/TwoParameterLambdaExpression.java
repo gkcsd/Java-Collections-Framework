93f949c24773bde3f95feb60e1e4d55cfc32eefc
gkcsd <kaleg2025@gmail.com>
2021-05-11 23:56:17 +0530

    TwoParameterLambdaExpression example added

---
+++ src/javalambda/TwoParameterLambdaExpression.java
@@ -0,0 +1,16 @@
+package javalambda;
+
+interface Test3 {
+    void print(Integer p1, Integer p2);
+}
+
+public class TwoParameterLambdaExpression {
+
+    static void fun(Test3 t, Integer p1, Integer p2) {
+        t.print(p1,p2);
+    }
+
+    public static void main(String[] args) {
+        fun((p1,p2) -> System.out.println(p1 + " " +p2),10, 20);
+    }
+}
