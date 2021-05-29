8ecd0e9f98c7db60fdbc5796cb8e3bce82a6ae24
gkcsd <kaleg2025@gmail.com>
2021-05-29 23:50:36 +0530

    LuckyFour example added

---
+++ src/PacticeProblems/LuckyFour.java
@@ -0,0 +1,73 @@
+package PacticeProblems;
+//Author- GKCSD
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class LuckyFour {
+    public static void main(String[] args) {
+        FastScanner fs = new FastScanner();
+        int T = fs.nextInt();
+        while (T-- > 0) {
+            int count = 0;
+            int num = fs.nextInt();
+            while(num!=0) {
+                int n = num%10;
+                num/=10;
+                if(n == 4) {
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        public String next() {
+            while (!st.hasMoreElements())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; ++i) a[i] = nextInt();
+            return a;
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        float nextFloat() {
+            return Float.parseFloat(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
