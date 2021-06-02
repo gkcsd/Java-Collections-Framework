37b4437bb821c5cdc0a2b6548d053349117af274
gkcsd <kaleg2025@gmail.com>
2021-06-02 22:12:16 +0530

    Remis added

---
+++ src/PacticeProblems/Remis.java
@@ -0,0 +1,87 @@
+package PacticeProblems;
+//GKCSD
+import java.io.*;
+import java.util.*;
+
+public class Remis {
+
+    private static void sol(FastScanner fs, PrintWriter out) throws IOException {
+        int a = fs.nextInt();
+        int b = fs.nextInt();
+        int min = 0;
+
+        if(a > b) {
+            min = a;
+        }else {
+            min = b;
+        }
+        int repo = a + b;
+        out.println(min + " " + repo);
+    }
+
+    public static void main(String[] args) throws IOException {
+        FastScanner fs = new FastScanner();
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+
+        int T = fs.nextInt();
+        while(T-- > 0) {
+            sol(fs, out);
+        }
+
+        out.flush();
+        fs.close();
+        out.close();
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastScanner(){
+            br=new BufferedReader(new InputStreamReader(System.in));
+            st = null;
+        }
+
+        public String next() {
+            while(st == null || !st.hasMoreElements())
+                try {
+                    st=new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine(){
+            String str = "";
+            try {
+                str = br.readLine();
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a=new int[n];
+            for(int i=0; i<n; ++i) a[i]=nextInt();
+            return a;
+        }
+
+        void close() throws IOException {
+            br.close();
+        }
+
+    }
+}
