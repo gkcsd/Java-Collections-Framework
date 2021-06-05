7629767e947345ab1ab892bf0e988bbe8da93621
gkcsd <kaleg2025@gmail.com>
2021-06-05 20:05:17 +0530

    BellaCiao example added

---
+++ src/PacticeProblems/BellaCiao.java
@@ -0,0 +1,89 @@
+package PacticeProblems;
+//GKCSD
+import java.io.*;
+import java.util.*;
+
+public class BellaCiao {
+
+    private static void sol(FastScanner fs, PrintWriter out) throws IOException {
+        int D = fs.nextInt(); //total no days hijack
+        int d = fs.nextInt(); // incrementation day interval
+        int P = fs.nextInt(); // printing money for each day
+        int Q = fs.nextInt(); // amount want to increase after interval
+        int count = 0;
+        int x = D/d;
+
+        if(D%d == 0) {
+            count = d * x * P + Q * (x - 1) * (x) * d / 2;
+        } else {
+            count = (d * x * P + Q*(x-1)*(x)*d/2) + (D%d) * (P+x*Q);
+        }
+        out.println(count);
+    }
+
+    public static void main(String[] args) throws IOException {
+        FastScanner fs = new FastScanner();
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+
+        int T = fs.nextInt();
+        while(T-- > 0) {
+            sol(fs, out);
+        }
+
+        out.flush();
+        fs.close();
+        out.close();
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastScanner(){
+            br=new BufferedReader(new InputStreamReader(System.in));
+            st = null;
+        }
+
+        public String next() {
+            while(st == null || !st.hasMoreElements())
+                try {
+                    st=new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine(){
+            String str = "";
+            try {
+                str = br.readLine();
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a=new int[n];
+            for(int i=0; i<n; ++i) a[i]=nextInt();
+            return a;
+        }
+
+        void close() throws IOException {
+            br.close();
+        }
+
+    }
+}
