5c055b506811864285a1d086c8e77d2e8725c558
gkcsd <kaleg2025@gmail.com>
2021-06-08 21:53:12 +0530

    WithoutGenerics example added

---
+++ src/arraylist/WithoutGenerics.java
@@ -0,0 +1,13 @@
+package arraylist;
+
+import java.util.ArrayList;
+
+public class WithoutGenerics {
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList();
+        al.add("Ganesh");
+        al.add(24);
+
+        System.out.println(al);
+    }
+}
