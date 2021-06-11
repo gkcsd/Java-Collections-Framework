e6517db2eb9f12283c1dd1907724bda9bc24f6e4
gkcsd <kaleg2025@gmail.com>
2021-06-11 21:05:41 +0530

    IncrementOrDecrement example added

---
+++ src/PacticeProblems/IncrementOrDecrement.java
@@ -0,0 +1,71 @@
+package PacticeProblems;
+//Author- GKCSD
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class IncrementOrDecrement {
+    public static void main(String[] args) {
+        FastScanner fs = new FastScanner();
+        int T = 1;
+        while (T-- > 0) {
+            int n = fs.nextInt();
+            if(n%4==0) {
+                n++;
+            }else{
+                n--;
+            }
+            System.out.println(n);
+        }
+    }
+
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        public String next() {
+            while (!st.hasMoreElements())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; ++i) a[i] = nextInt();
+            return a;
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        float nextFloat() {
+            return Float.parseFloat(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
