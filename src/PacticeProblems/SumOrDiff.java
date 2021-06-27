3c5767fca44a3de953f3265f4d4b4ae8ebc14702
gkcsd <kaleg2025@gmail.com>
2021-06-27 21:43:33 +0530

    SumOrDiff example added

---
+++ src/PacticeProblems/SumOrDiff.java
@@ -0,0 +1,81 @@
+package PacticeProblems;
+//GKCSD
+import java.io.*;
+import java.util.*;
+
+public class SumOrDiff {
+
+    private static void sol(FastScanner fs, PrintWriter out) throws IOException {
+        int a=fs.nextInt();
+        int b=fs.nextInt();
+
+        int res = (a>b) ? a-b : a+b;
+        out.println(res);
+    }
+
+    public static void main(String[] args) throws IOException {
+        FastScanner fs = new FastScanner();
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+
+        int T = 1;
+        while(T-- > 0) {
+            sol(fs, out);
+        }
+
+        out.flush();
+        fs.close();
+        out.close();
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastScanner(){
+            br=new BufferedReader(new InputStreamReader(System.in));
+            st = null;
+        }
+
+        public String next() {
+            while(st == null || !st.hasMoreElements())
+                try {
+                    st=new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine(){
+            String str = "";
+            try {
+                str = br.readLine();
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a=new int[n];
+            for(int i=0; i<n; ++i) a[i]=nextInt();
+            return a;
+        }
+
+        void close() throws IOException {
+            br.close();
+        }
+
+    }
+}
