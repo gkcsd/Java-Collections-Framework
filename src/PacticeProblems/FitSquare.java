27f26886830666bca367603414c1bc21486a030b
gkcsd <kaleg2025@gmail.com>
2021-07-08 22:17:31 +0530

    FitSquare example added

---
+++ src/PacticeProblems/FitSquare.java
@@ -0,0 +1,68 @@
+package PacticeProblems;
+//Author- GKCSD
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class FitSquare {
+    public static void main(String[] args) {
+        FastScanner fs = new FastScanner();
+        int T = fs.nextInt();
+        while (T-- > 0) {
+            int b=fs.nextInt();
+            int base = Math.floorDiv((b-2),2);
+            int res=(base * (base + 1)) / 2;
+            System.out.println(res);
+        }
+    }
+
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        public String next() {
+            while (!st.hasMoreElements())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; ++i) a[i] = nextInt();
+            return a;
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        float nextFloat() {
+            return Float.parseFloat(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
