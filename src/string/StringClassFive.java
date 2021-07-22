c525f17ef76ae4f617d830d16342d762e937f8f2
gkcsd <kaleg2025@gmail.com>
2021-07-22 14:39:21 +0530

    StringClassFive example added

---
+++ src/string/StringClassFive.java
@@ -0,0 +1,10 @@
+package string;
+
+public class StringClassFive {
+    public static void main(String[] args) {
+        String s1 = "GaneshForAnuForGanesh";
+        String s2 = "Ganesh";
+
+        System.out.println(s1.indexOf(s2, 1));
+    }
+}
