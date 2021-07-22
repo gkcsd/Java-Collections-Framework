066e9c76b60a1e0a0aa462373bae4c8a505f4597
gkcsd <kaleg2025@gmail.com>
2021-07-22 14:38:35 +0530

    StringClassThree example added

---
+++ src/string/StringClassThree.java
@@ -0,0 +1,12 @@
+package string;
+
+public class StringClassThree {
+    public static void main(String[] args) {
+        String s1 = "Ganesh";
+        String s2 = "Gan";
+
+        System.out.println(s1.contains(s2));
+
+        System.out.println(s1.equals(s2));
+    }
+}
