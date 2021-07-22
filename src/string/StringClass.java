8a258203f0bdc55b9c823e868dc1655e9cd56e73
gkcsd <kaleg2025@gmail.com>
2021-07-22 14:38:07 +0530

    StringClass example added

---
+++ src/string/StringClass.java
@@ -0,0 +1,12 @@
+package string;
+
+public class StringClass {
+    public static void main(String[] args) {
+        String str = "Ganesh";
+
+        System.out.println(str.length());
+        System.out.println(str.charAt(3));
+        System.out.println(str.substring(2));
+        System.out.println(str.substring(2,5));
+    }
+}
