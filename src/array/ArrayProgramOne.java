261972f886a0dd1ea76a7760566064bf91824264
gkcsd <kaleg2025@gmail.com>
2021-07-23 12:40:33 +0530

    ArrayProgramOne example added

---
+++ src/array/ArrayProgramOne.java
@@ -0,0 +1,14 @@
+package array;
+
+import java.util.Arrays;
+
+public class ArrayProgramOne {
+    public static void main(String[] args) {
+        int a[] = {10,20,30};
+        int b[] = {10,20,30};
+
+        System.out.println(Arrays.equals(a,b));
+
+        System.out.println(Arrays.toString(a));
+    }
+}
